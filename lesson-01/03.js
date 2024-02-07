4c53336d203059b3ad6398728f1531709cc4f409
Chaslen2101 <Chaslen2101@gmail.com>
2024-02-08 01:53:38 +0300

    1

--- lesson-01/03.js
+++ lesson-01/03.js
@@ -1,8 +1,5 @@
 let number = 1;
 console.log(number++); //1
-number++;
-console.log(number++); //3
-number--
+console.log(++number); //3
 console.log(number--); //3
-number--;
-console.log(number--);
+console.log(--number); //1
