3fd867a265c2bad05b42bea57b7a4aa7c2c3b2cd
Chaslen2101 <Chaslen2101@gmail.com>
2024-02-07 06:57:35 +0300

    homeworkJS_L1_02_Changes

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,6 +1,5 @@
 let number1 = Math.floor(Math.random() * 100);
-let numberCheck = Number.isInteger(number1 / 2);
-if ( numberCheck == true) {
+if (number1 % 2 == 0) {
     console.log(number1, ':Это четное число');
 } else {
     console.log(number1, ':Это нечетное число');
