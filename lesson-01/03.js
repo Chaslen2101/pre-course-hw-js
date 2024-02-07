3d8f34588e9ce8a57755c188f867fb908fc80aff
Chaslen2101 <Chaslen2101@gmail.com>
2024-02-07 07:35:41 +0300

    homeworkJS_L1_03_Changes

---
+++ lesson-01/03.js
@@ -0,0 +1,11 @@
+let number = 1;
+console.log(number);
+while (number != 3) {
+    number++
+};
+console.log(number);
+console.log(number);
+while (number != 1) {
+    number--
+};
+console.log(number);
