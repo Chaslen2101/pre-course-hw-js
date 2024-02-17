bb302759ac1f41635e53e6038e7ec7b5b27866ea
Chaslen2101 <Chaslen2101@gmail.com>
2024-02-17 05:32:42 +0300

    homeworkJS_L2_07_04

--- lesson-02/07.js
+++ lesson-02/07.js
@@ -7,15 +7,8 @@ let passportMarried = {
     }
 };
 
-let passportMarried2 = {
-    name: "Petr",
-    surname: "Petrov",
-    address: {
-        country: "USA",
-        city: "Bobryisk"
-    },
-    married: true
-};
+let passportMarried2 = structuredClone(passportMarried);
+passportMarried2.married = true;
 
 console.log (passportMarried);
 console.log (passportMarried2);
