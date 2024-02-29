0875388fafadfb58631e89a4c95aa999b39aa900
Chaslen2101 <Chaslen2101@gmail.com>
2024-02-29 03:49:23 +0300

    homeworkJS_L3_01_01

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,8 @@
-// code
+let colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+function createColorString() {
+    return colors = colors.filter(item => item == "черный" || item == "красный" || item == "желтый").join('-');
+}
+
+console.log(createColorString());
+
